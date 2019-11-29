fced1bb6239804336e7ef1cca86e5140d5d34451
Romain Lorrilliere <romainlorrilliere@gmail.com>
2019-11-29 23:26:32 +0100

    Update description_birds.r

--- description_birds.r
+++ description_birds.r
@@ -1,3 +1,6 @@
+
+## 2018
+
 data_directory <- "data/"
 data_file <- "bird_with_names.csv"
 
@@ -5,7 +8,7 @@ filedata <- paste(data_directory,data_file,sep="")
 filesp <- paste(data_directory,"espece.csv",sep="")
 dsp <- read.csv("data/espece.csv",encoding="UTF-8")
 
-d <- read.csv2(filedata)
+d <-  read.csv2(filedata)
 spfr <- c(as.character(unique(subset(d,city=="Paris")$french_name)))
 
 ab_sp <- aggregate(abondance~french_name+park_id+site+city,d,sum,na.rm=TRUE)
@@ -32,3 +35,57 @@ ddiv$isParis <- ddiv$city == "Paris"
 gg <- ggplot(ddiv,aes(x=div,group=isParis,fill=isParis))+geom_histogram(alpha=.75)
 gg <- gg + labs(title="Diversité dans les parcs")
 ggsave("div_parcs.png",gg,width=7,height=7)
+
+
+
+
+## ----------------------------------------------------------------------
+
+## 2019
+
+library(data.table)
+library(dplyr)
+
+data_directory <- "data/"
+data_file0 <- "bird_with_names.csv"
+data_file <- "bird_city_datasheet_FR.csv"
+
+filedata0 <- paste(data_directory,data_file0,sep="")
+filedata <- paste(data_directory,data_file,sep="")
+filesp <- paste(data_directory,"espece.csv",sep="")
+
+dsp <- fread("data/espece.csv",encoding="UTF-8")
+d0 <- fread(filedata0)
+d <-  fread(filedata)
+
+spsc <- dsp[,c("pk_species","scientific_name"),with=FALSE]
+colnames(spsc)[1] <- "sp"
+
+d0 <- d0[passage > 0]
+d0 <- unique(d0[,c("park_id","latitude_wgs84","longitude_wgs84"),with=FALSE])
+d0 <- d0[park_id == "Pa492bis",park_id := "Pa492"]
+colnames(d0)[2:3] <- c("lat","lon")
+
+colonnes <- colnames(d)
+dim(d)
+dim(d0)
+
+d <- data.table(inner_join(d,d0))
+dim(d)
+d[is.na(lat)]
+d[is.na(sp)]
+
+
+d <- d[,latitude_wgs84 := lat]
+d <- d[,longitude_wgs84 := lon]
+
+
+d <- data.table(inner_join(d,spsc))
+dim(d)
+
+d[is.na(scientific_name)]
+d <- d[,sp := scientific_name]
+
+d <- d[,colonnes,with=FALSE]
+
+write.csv(d,"data/bird_city_datasheet_FR_Paris.csv",row.names=FALSE)
